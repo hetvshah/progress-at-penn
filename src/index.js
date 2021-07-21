03aef3a0117743786811d16b8b4ed4ab866cc896
Hetvi Shah <hetvi2@gmail.com>
2021-07-21 08:32:53 -0500

    base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/progress-at-penn">
+    <HashRouter basename="/">
       <App />
     </HashRouter>
   </React.StrictMode>,
