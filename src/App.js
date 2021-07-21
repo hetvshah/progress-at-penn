c4e70b1f48398829bbb0f6721133a0f493957a37
Hetvi Shah <hetvi2@gmail.com>
2021-07-21 08:12:57 -0500

    resolving router bug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home.js';
 import Info from './components/Info';
 
 function App() {
   return (
-    <HashRouter basename="/">
-      <Switch>
-        <Route exact path="/" render={(props) => <Home {...props} />} />
-        <Route exact path="/info" render={(props) => <Info {...props} />} />
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <Route exact path="/" render={(props) => <Home {...props} />} />
+      <Route exact path="/info" render={(props) => <Info {...props} />} />
+    </Switch>
   );
 }
 
